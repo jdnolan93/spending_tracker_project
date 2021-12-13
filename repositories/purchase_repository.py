a8992fcc9d8cabc5b461d00d100553d6666dbdde
Jamie <jamienolan93@hotmail.co.uk>
2021-12-13 12:34:25 +0000

    added save to purchase repository

--- repositories/purchase_repository.py
+++ repositories/purchase_repository.py
@@ -2,3 +2,13 @@ from db.run_sql import run_sql
 
 from models.place import Place
 from models.purchase import Purchase
+import repositories.place_repository as place_repository
+
+def save(purchase):
+    sql = "INSERT INTO purchases (item_name, price, place_id) VALUES (%s, %s, %s) RETURNING *"
+    values = [purchase.item_name, purchase.price, purchase.place.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    purchase.id = id
+    return purchase
+
