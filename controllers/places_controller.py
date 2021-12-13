8cfc8437d7bf9facc889830fcd4373ab42af5642
Jamie <jamienolan93@hotmail.co.uk>
2021-12-13 23:53:58 +0000

    added tags controller

--- controllers/places_controller.py
+++ controllers/places_controller.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from models.place import Place
 import repositories.place_repository as place_repository
 import repositories.purchase_repository as purchase_repository
+import repositories.tag_repository as tag_repository
 
 places_blueprint = Blueprint("places", __name__)
 
--- controllers/tags_controller.py
+++ controllers/tags_controller.py
@@ -5,4 +5,4 @@ import repositories.place_repository as place_repository
 import repositories.purchase_repository as purchase_repository
 import repositories.tag_repository as tag_repository
 
-purchases_blueprint = Blueprint("purchases", __name__)
+tags_blueprint = Blueprint("tags", __name__)
