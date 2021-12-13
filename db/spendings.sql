39621582cc0cfa884c1b89250b97c672509c21d2
Jamie <jamienolan93@hotmail.co.uk>
2021-12-13 10:07:52 +0000

    fixed a couple syntax errors in tables (commas, semicolons)

--- db/spendings.sql
+++ db/spendings.sql
@@ -4,11 +4,11 @@ DROP TABLE IF EXISTS places;
 CREATE TABLE places(
     id SERIAL PRIMARY KEY,
     place_name VARCHAR(255)
-)
+);
 
 CREATE TABLE purchases(
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
     item_name VARCHAR(255),
     price FLOAT(2),
     place_id INT REFERENCES places(id)
-)
+);
