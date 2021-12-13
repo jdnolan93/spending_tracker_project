f0cc1b1a879ac5be3ebc9899fc18551482ca2b6d
Jamie <jamienolan93@hotmail.co.uk>
2021-12-13 09:57:08 +0000

    created tables in spendings.sql

--- models/purchase.py
+++ models/purchase.py
@@ -1,5 +1,6 @@
 class Purchase:
-    def __init__(self, item_name, price, id=None, ):
+    def __init__(self, item_name, price, place id=None, ):
         self.item_name=item_name
         self.price=price
+        self.place=place
         id=None
