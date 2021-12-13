c6a08c1046a94b71501fbe2fe33cf63ad2979018
Jamie <jamienolan93@hotmail.co.uk>
2021-12-13 11:05:26 +0000

    created test files and entered basic setup

---
+++ tests/place_test.py
@@ -0,0 +1,5 @@
+import unittest
+
+from models.place import Place
+
+class TestTask(unittest.TestCase):
---
+++ tests/purchase_test.py
@@ -0,0 +1,5 @@
+import unittest
+
+from models.purchase import Purchase
+
+class TestTask(unittest.TestCase):
