4a804c8daf8c0d0698bbb04e2942d0d6d7fee809
Jamie <jamienolan93@hotmail.co.uk>
2021-12-13 09:58:09 +0000

    fixed error in purchase.py

--- models/purchase.py
+++ models/purchase.py
@@ -1,6 +1,7 @@
 class Purchase:
-    def __init__(self, item_name, price, place id=None, ):
+    def __init__(self, item_name, price, place, id=None, ):
         self.item_name=item_name
         self.price=price
         self.place=place
         id=None
+
