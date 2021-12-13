b678eabec21276fa7abf0dd18b327b81b19cedb9
Jamie <jamienolan93@hotmail.co.uk>
2021-12-13 10:58:09 +0000

    added save to place repository

--- repositories/place_repository.py
+++ repositories/place_repository.py
@@ -2,3 +2,11 @@ from db.run_sql import run_sql
 
 from models.place import Place
 from models.purchase import Purchase
+
+def save(place):
+    sql = "INSERT INTO places (place_name) VALUES (%s) RETURNING *"
+    values = [place.place_name]
+    results = run_sql(sql, values)
+    id = results [0] ['id']
+    place.id = id
+    return place
