2e3649b2f1d9d599030bd77401ab1921ea649091
Jamie <jamienolan93@hotmail.co.uk>
2021-12-13 11:28:32 +0000

    added price test

--- tests/purchase_test.py
+++ tests/purchase_test.py
@@ -4,7 +4,11 @@ from models.purchase import Purchase
 class TestPurchase(unittest.TestCase):
 
     def setUp(self):
-        self.purchase = Purchase("hat", "3.33", "Woolworths")
+        self.purchase = Purchase("hat", 3.33, "Woolworths")
 
     def test_if_has_name(self):
         self.assertEqual("hat", self.purchase.item_name)
+
+    def test_if_has_price(self):
+        self.assertEqual(3.33, self.purchase.price)
+        
