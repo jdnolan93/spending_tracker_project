bd5af8e5358f52d4356c0dd9b564c82f10483bde
Jamie <jamienolan93@hotmail.co.uk>
2021-12-14 00:08:38 +0000

    added tag to console.py

--- console.py
+++ console.py
@@ -2,9 +2,11 @@ import pdb
 
 from models.place import Place
 from models.purchase import Purchase
+from models.tag import Tag
 
 import repositories.place_repository as place_repository
-import repositories.purchase_repository as purchase_repository
+import repositories.purchase_repository as purchase_repository 
+import repositories.tag_repository as tag_repository
 
 place_repository.delete_all()
 purchase_repository.delete_all()
@@ -18,24 +20,18 @@ place_repository.save(place_2)
 place_3 = Place("Tesco")
 place_repository.save(place_3)
 
-place_4 = Place("The Hive")
-place_repository.save(place_4)
+tag_1 = Tag("Pint", True)
 
-place_5 = Place("Sainsburys")
-place_repository.save(place_5)
+tag_2 = Tag("Steak Bake", False)
 
+tag_3 = Tag("Meal Deal", False)
 
-purchase_1 = Purchase("Pint", 3.50, place_1)
+purchase_1 = Purchase(3.50, place_1, tag_1)
 purchase_repository.save(purchase_1)
 
-purchase_2 = Purchase("Steak Bake", 1.50, place_2)
+purchase_2 = Purchase(1.50, place_2, tag_2)
 purchase_repository.save(purchase_2)
 
-purchase_3 = Purchase("Meal Deal", 3.00, place_3)
+purchase_3 = Purchase(3.00, place_3, tag_3)
 purchase_repository.save(purchase_3)
 
-purchase_4 = Purchase("Vodka mixer", 1.00, place_4)
-purchase_repository.save(purchase_4)
-
-purchase_5 = Purchase("Irn Bru", 0.85, place_5)
-purchase_repository.save(purchase_5)
