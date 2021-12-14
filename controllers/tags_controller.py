025c536c9e1e61377f88795959e77c92a6db74ab
Jamie <jamienolan93@hotmail.co.uk>
2021-12-14 12:11:49 +0000

    added new and create to tags controller

--- controllers/tags_controller.py
+++ controllers/tags_controller.py
@@ -11,3 +11,14 @@ tags_blueprint = Blueprint("tags", __name__)
 def tags():
     tags = tag_repository.select_all()
     return render_template("tags/index.html", tags = tags)
+
+@tags_blueprint.route("/tags/new")
+def new_tag():
+    return render_template("/tags/new.html")
+
+@tags_blueprint.route("/tags", methods=["POST"])
+def create_tag():
+    tag_name = request.form["name"]
+    new_tag = Tag(tag_name)
+    tag_repository.save(new_tag)
+    return redirect("/tags")
