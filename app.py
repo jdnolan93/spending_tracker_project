4ef8c07e94c3da89b141762113b14a062e577934
Jamie <jamienolan93@hotmail.co.uk>
2021-12-14 11:47:14 +0000

    updated tags html

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 from controllers.places_controller import places_blueprint
 from controllers.purchases_controller import purchases_blueprint
+from controllers.tags_controller import tags_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(places_blueprint)
 app.register_blueprint(purchases_blueprint)
+app.register_blueprint(tags_blueprint)
 
 @app.route('/')
 def home():
--- controllers/tags_controller.py
+++ controllers/tags_controller.py
@@ -6,3 +6,8 @@ import repositories.purchase_repository as purchase_repository
 import repositories.tag_repository as tag_repository
 
 tags_blueprint = Blueprint("tags", __name__)
+
+@tags_blueprint.route("/tags")
+def tags():
+    tags = tag_repository.select_all()
+    return render_template("tags/index.html", tags = tags)
